ac5c0f1503d89b26f3818a7196cbcba0924d94c2
HP <umarqulovmirjalol&@gmail.com>
2023-01-07 20:10:35 +0500

    int main project

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Helllo world");
+        System.out.println(sum(5,7));
+    }
+    public static int sum(int a, int b){
+        return a+b;
+    }
+}
